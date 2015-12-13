fd36a0df13784d310559143691841bd7ee96dd56
Brian Coe <coesurf@Brians-MacBook-Pro.local>
2015-12-13 13:15:43 -0800

    root changed to coesurf.html

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ var SampleApp = function() {
 
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
+            res.send(self.cache_get('coesurf.html') );
         };
         
         self.routes['/styles.css'] = function(req, res) {
