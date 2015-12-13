822b624e1f20577a65733e5d50281f611f9b1b3a
Brian Coe <coesurf@Brians-MacBook-Pro.local>
2015-12-13 13:44:35 -0800

    detect hostname for elasticgui

--- server.js
+++ server.js
@@ -105,7 +105,11 @@ var SampleApp = function() {
 
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('coesurf.html') );
+            if (req.headers.host.indexOf('elasticgui') > -1) {
+               res.send(self.cache_get('elasticgui.html');
+            } else {
+               res.send(self.cache_get('coesurf.html') );
+            }
         };
         
         self.routes['/styles.css'] = function(req, res) {
